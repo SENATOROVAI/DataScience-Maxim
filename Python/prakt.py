7027e3c87c1a7a10f68b3ceacf53ff1d917e7d02
zatomaks <mr.maks.pupkin.@mail.ru>
2024-01-19 16:30:41 +0300

    +дз алг

--- Python/prakt.py
+++ Python/prakt.py
@@ -1,13 +1,4 @@
-sp = [3, 2, 4]
-tar = 6
-s = 0
-for i in range(len(sp)):
-    sp1 = sp.copy()
-    sp1.pop(i)
-    for j in range(len(sp1)):
-        if sp[i] + sp1[j] == tar:
-            print([i, sp.index(sp1[j])])
-            s += 1
-            break
-    if s == 1:
-        break
+import bisect
+sp = [1, 2, 4]
+k = bisect.bisect(sp, 3)
+print(k)
