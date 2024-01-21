4cf38fc3c0e04aae8ea6748ca8cfd2789698aa45
zatomaks <mr.maks.pupkin.@mail.ru>
2024-01-21 20:57:15 +0300

    + дз

--- Python/prakt.py
+++ Python/prakt.py
@@ -1,3 +1,9 @@
-sl = 'internationalization'
-if len(sl) > 10:
-        print(sl[0] + str(len(sl[1:len(sl) - 1])) + sl[-1])
+
+def reverse(x: int) -> int:
+    if '-' in str(x):
+        ch = str(x)[str(x).find('-'):]
+        return int(ch[::-1]) * -1
+    else:
+        return int(str(x)[::-1])
+print(reverse(-123))
+        
