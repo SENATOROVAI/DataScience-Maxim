6da8de0ef11d1c08aa8d8fecb55b927323f611d4
zatomaks <mr.maks.pupkin.@mail.ru>
2024-01-22 23:25:36 +0300

    +дз

--- Python/prakt.py
+++ Python/prakt.py
@@ -1,9 +1,8 @@
+sp = [2, 3, 8, 6, 45, 32, 56, 32, 14, 25]
+sp1 = [0]
+[2, 5, 13, 19, 64, 96, 152, 184, 198, 223]
 
-def reverse(x: int) -> int:
-    if '-' in str(x):
-        ch = str(x)[str(x).find('-'):]
-        return int(ch[::-1]) * -1
-    else:
-        return int(str(x)[::-1])
-print(reverse(-123))
-        
+sp2 = [sum(sp[:i]) + sp[i + 1] for i in range(len(sp))]
+for i in range(len(sp)):
+    sp1.append(sp[i] + sp1[i])
+sp1.pop(0)
