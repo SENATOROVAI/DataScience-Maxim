6b93c8084a7f235cf6f1ee4c97a7a16b460cfdc7
zatomaks <mr.maks.pupkin.@mail.ru>
2024-01-28 23:15:53 +0300

    +дз

--- Python/prakt.py
+++ Python/prakt.py
@@ -1,9 +1,9 @@
-from typing import List
-sp = [2, 3, 8, 6, 45, 32, 56, 32, 14, 25]
-sp1 = [0]
-[2, 5, 13, 19, 64, 96, 152, 184, 198, 223]
-
-sp2: List = [sum(sp[:i]) + sp[i + 1] for i in range(len(sp))]
-for i in range(len(sp)):
-    sp1.append(sp[i] + sp1[i])
-sp1.pop(0)
+def isValid(s: str):
+    sl: dict = {}
+    for i in s:
+        sl[i] = sl.get(i, 0) + 1
+    if sl['('] == sl[')'] and sl['['] == sl[']'] and sl['{'] == sl['}']:
+        return True
+    else:
+        return False
+print(isValid('()[]{}}'))
