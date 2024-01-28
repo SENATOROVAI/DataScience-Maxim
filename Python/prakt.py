cbeeb7846f6b393a2c066a77a43646dc842503b9
zatomaks <mr.maks.pupkin.@mail.ru>
2024-01-28 16:22:23 +0300

    Урок

--- Python/prakt.py
+++ Python/prakt.py
@@ -1,8 +1,9 @@
+from typing import List
 sp = [2, 3, 8, 6, 45, 32, 56, 32, 14, 25]
 sp1 = [0]
 [2, 5, 13, 19, 64, 96, 152, 184, 198, 223]
 
-sp2 = [sum(sp[:i]) + sp[i + 1] for i in range(len(sp))]
+sp2: List = [sum(sp[:i]) + sp[i + 1] for i in range(len(sp))]
 for i in range(len(sp)):
     sp1.append(sp[i] + sp1[i])
 sp1.pop(0)
