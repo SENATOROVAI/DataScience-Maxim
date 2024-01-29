bc121648480547874cb1574de5c932d9bf98da45
zatomaks <mr.maks.pupkin.@mail.ru>
2024-01-29 18:31:45 +0300

    Co-authored-by: Ruslan Senatorov <ruslansenatorov@users.noreply.github.com>

--- Python/prakt.py
+++ Python/prakt.py
@@ -1,9 +1,27 @@
-def isValid(s: str):
-    sl: dict = {}
-    for i in s:
-        sl[i] = sl.get(i, 0) + 1
-    if sl['('] == sl[')'] and sl['['] == sl[']'] and sl['{'] == sl['}']:
-        return True
-    else:
-        return False
-print(isValid('()[]{}}'))
+products = {
+    "ноутбук": 5000,
+    "смартфон": 20000,
+    "наушники": 1000,
+    "монитор": 10000,
+    "клавиатура": 500,
+    "мышь": 200,
+    "роутер": 1500,
+    "принтер": 5000,
+    "флешка": 1000,
+    "жесткий диск": 3000
+}
+def calculate_order_cost(products, *args):
+    sl = {}
+    for i in args:
+        if i in products.keys():
+            sl[i] = sl.get(i, 0) + 1
+    sm = 0
+    for v, k in sl.items():
+        sm += v * products[k]
+    return sl
+    
+    
+    return sl
+total_cost= calculate_order_cost(products, 'ноутбук', 'роутер') 
+print(total_cost) # 6500
+
