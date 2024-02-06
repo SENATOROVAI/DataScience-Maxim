5c7f4bfca6bcd2fda15bba5d901331aa3a2d81e0
zatomaks <mr.maks.pupkin.@mail.ru>
2024-02-06 23:30:21 +0300

    +

--- Python/prakt.py
+++ Python/prakt.py
@@ -1,11 +1,7 @@
-# Пример 1
-numbers_list = [1, 1, 2, 2, 3, 3, 4, 4]
-
-numbers_list_ordered = sorted(numbers_list, reverse=True)
-numbers_set = set(numbers_list)
-numbers_set.add(max(numbers_list) + 1)
-
-b = set(numbers_list) - {min(numbers_list), }
-numbers_frozenset = frozenset(b)
-print(numbers_set)
-
+n = int(input())
+c = 0
+for i in range(5, 0, -1):
+    while n - i >= 0:
+        n -= i
+        c += 1
+print(c)
