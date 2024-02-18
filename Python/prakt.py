fb5398c1295e6bf5ad51e3c04e9a4cf332bea9f1
zatomaks <mr.maks.pupkin.@mail.ru>
2024-02-18 23:16:49 +0300

    +

--- Python/prakt.py
+++
@@ -1,9 +0,0 @@
-workers = [['Ivan', 'Ivanov', 100000, 2], ['Petr', 'Petrov', 150000, 2], ['Sidor', 'Sidorov', 200000, 3]]
-for i in workers:
-    if i[3] < 2:
-        i[3] = 'junior'
-    elif 2 <= i[3] <= 5:
-        i[3] = 'middle'
-    else:
-        i[3] = 'senior'
-    print(i[0] + ' ' +  i[1] + ' is ' + i[3])
