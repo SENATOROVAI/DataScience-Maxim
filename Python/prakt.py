e4e0839fbb397c97efb4f37a72aa5395af998784
zatomaks <mr.maks.pupkin.@mail.ru>
2024-02-28 11:29:52 +0300

    +

--- Python/prakt.py
+++ Python/prakt.py
@@ -1,8 +1,2 @@
-class Solution:
-    def camelMatch(self, queries: List[str], pattern: str) -> List[bool]:
-        b = list(filter(lambda x: x.isupper(), list(pattern)))
-        for i in queries:
-            for j in b:
-                if i.index(j) >= b.index(j) and 
-            
+
                 
